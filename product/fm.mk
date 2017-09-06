b927f6168c0bef2db9c025d7a41f530ccbd6ff15
Murat Kozan <muratkozan300@gmail.com>
2017-09-06 23:31:23 +0300

    Update fm.mk

--- product/fm.mk
+++ product/fm.mk
@@ -4,9 +4,9 @@ PRODUCT_PACKAGES += \
     libfmjni
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/fmr/mt6631_fm_cust.cfg:system/etc/fmr/mt6631_fm_cust.cfg \
-	$(LOCAL_PATH)/prebuilt/FM/lib/libfmcust.so:system/lib/libfmcust.so \
-	$(LOCAL_PATH)/prebuilt/FM/lib64/libfmcust.so:system/lib64/libfmcust.so \
-	$(LOCAL_PATH)/prebuilt/FM/lib/libmtkplayer.so:system/lib/libmtkplayer.so \
-	$(LOCAL_PATH)/prebuilt/FM/lib64/libmtkplayer.so:system/lib64/libmtkplayer.so
+        device/doogee/f7/configs/fmr/mt6631_fm_cust.cfg:system/etc/fmr/mt6631_fm_cust.cfg \
+	device/doogee/f7/prebuilt/FM/lib/libfmcust.so:system/lib/libfmcust.so \
+	device/doogee/f7/prebuilt/FM/lib64/libfmcust.so:system/lib64/libfmcust.so \
+	device/doogee/f7/prebuilt/FM/lib/libmtkplayer.so:system/lib/libmtkplayer.so \
+	device/doogee/f7/prebuilt/FM/lib64/libmtkplayer.so:system/lib64/libmtkplayer.so
 
