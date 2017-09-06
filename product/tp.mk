260bb02e5347b6e4ede54fefe307c198ee70e274
Murat Kozan <muratkozan300@gmail.com>
2017-09-07 00:19:08 +0300

    Update tp.mk

--- product/tp.mk
+++ product/tp.mk
@@ -1,12 +1,12 @@
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/tp/etc/.tp/.ht120.mtc:system/etc/.tp/.ht120.mtc \
-	$(LOCAL_PATH)/tp/etc/.tp/.thermal_policy_00:system/etc/.tp/.thermal_policy_00 \
-	$(LOCAL_PATH)/tp/etc/.tp/.thermal_policy_01:system/etc/.tp/.thermal_policy_01 \
-	$(LOCAL_PATH)/tp/etc/.tp/.thermal_policy_02:system/etc/.tp/.thermal_policy_02 \
-	$(LOCAL_PATH)/tp/etc/.tp/.thermal_policy_03:system/etc/.tp/.thermal_policy_03 \
-	$(LOCAL_PATH)/tp/etc/.tp/thermal.conf:system/etc/.tp/thermal.conf \
-	$(LOCAL_PATH)/tp/etc/.tp/thermal.off.conf:system/etc/.tp/thermal.off.conf \
-	$(LOCAL_PATH)/tp/bin/thermal:system/bin/thermal \
-	$(LOCAL_PATH)/tp/bin/thermal_manager:system/bin/thermal_manager \
-	$(LOCAL_PATH)/tp/bin/thermald:system/bin/thermald \
-	$(LOCAL_PATH)/tp/bin/thermalloadalgod:system/bin/thermalloadalgod
+	device/doogee/f7/tp/etc/.tp/.ht120.mtc:system/etc/.tp/.ht120.mtc \
+	device/doogee/f7/tp/etc/.tp/.thermal_policy_00:system/etc/.tp/.thermal_policy_00 \
+	device/doogee/f7/tp/etc/.tp/.thermal_policy_01:system/etc/.tp/.thermal_policy_01 \
+	device/doogee/f7/tp/etc/.tp/.thermal_policy_02:system/etc/.tp/.thermal_policy_02 \
+	device/doogee/f7/tp/etc/.tp/.thermal_policy_03:system/etc/.tp/.thermal_policy_03 \
+	device/doogee/f7/tp/etc/.tp/thermal.conf:system/etc/.tp/thermal.conf \
+	device/doogee/f7//tp/etc/.tp/thermal.off.conf:system/etc/.tp/thermal.off.conf \
+	device/doogee/f7/tp/bin/thermal:system/bin/thermal \
+	device/doogee/f7//tp/bin/thermal_manager:system/bin/thermal_manager \
+	device/doogee/f7/tp/bin/thermald:system/bin/thermald \
+	device/doogee/f7/tp/bin/thermalloadalgod:system/bin/thermalloadalgod
