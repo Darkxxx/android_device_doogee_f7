10fdd3f3940a81350a30dd6f83a44c65c252ee76
Murat Kozan <muratkozan300@gmail.com>
2017-09-07 14:18:11 +0300

    Delete gps.mk

--- product/gps.mk
+++
@@ -1,5 +0,0 @@
-# GPS
-PRODUCT_COPY_FILES += \
-    device/doogee/f7/configs/slp_conf:system/etc/slp_conf \
-    device/doogee/f7/configs/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
