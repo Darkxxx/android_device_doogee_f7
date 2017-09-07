41151d568d4aeaee37b75a9969a7f392760fc5dd
Murat Kozan <muratkozan300@gmail.com>
2017-09-07 15:49:40 +0300

    Delete audio.mk

--- board/audio.mk
+++
@@ -1,6 +0,0 @@
-# MTK audio
-BOARD_USES_MTK_AUDIO := true
-
-PRODUCT_COPY_FILES += \
-        device/doogee/f7/prebuilt/audio/lib/hw/audio.primary.mt6797.so:system/lib/hw/audio.primary.mt6797.so \
-	device/doogee/f7/prebuilt/audio/lib64/hw/audio.primary.mt6797.so:system/lib64/hw/audio.primary.mt6797.so
