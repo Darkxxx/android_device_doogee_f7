cf64392b67b0772f1fee7a04104e9bb555256fed
Murat Kozan <muratkozan300@gmail.com>
2017-09-07 14:20:10 +0300

    Delete input.mk

--- product/input.mk
+++
@@ -1,4 +0,0 @@
-# Keyboard layout
-PRODUCT_COPY_FILES += \
-     device/doogee/f7/keylayout/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl \
-     device/doogee/f7/keylayout/ACCDET.kl:system/usr/keylayout/ACCDET.kl
