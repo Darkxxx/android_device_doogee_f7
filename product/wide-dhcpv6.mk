9e623832db5e7006b53d58ec758c1112d371bac2
Murat Kozan <muratkozan300@gmail.com>
2017-09-07 14:33:00 +0300

    Delete wide-dhcpv6.mk

--- product/wide-dhcpv6.mk
+++
@@ -1,8 +0,0 @@
-PRODUCT_COPY_FILES += \
-    device/doogee/f7/configs/wide-dhcpv6/dhcp6c.conf:system/etc/wide-dhcpv6/dhcp6c.conf \
-    device/doogee/f7/configs/wide-dhcpv6/dhcp6c.script:system/etc/wide-dhcpv6/dhcp6c.script \
-    device/doogee/f7/configs/wide-dhcpv6/dhcp6cctlkey:system/etc/wide-dhcpv6/dhcp6cctlkey \
-    device/doogee/f7/configs/wide-dhcpv6/dhcp6cDNS.conf:system/etc/wide-dhcpv6/dhcp6cDNS.conf \
-    device/doogee/f7/configs/wide-dhcpv6/dhcp6cPD.conf:system/etc/wide-dhcpv6/dhcp6cPD.conf \
-    device/doogee/f7/configs/wide-dhcpv6/dhcp6cPD.script:system/etc/wide-dhcpv6/dhcp6cPD.script \
-    device/doogee/f7/configs/wide-dhcpv6/dhcp6s.conf:system/etc/wide-dhcpv6/dhcp6s.conf
