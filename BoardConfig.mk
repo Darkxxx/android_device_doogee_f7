6b3df5efa982ce018a44425253f264fd676feb8e
Murat Kozan <muratkozan300@gmail.com>
2017-09-07 21:33:06 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,8 +18,36 @@
 LOCAL_PATH := device/doogee/f7
 TARGET_SPECIFIC_HEADER_PATH := device/doogee/f7/include
 
-# Device board elements
-include device/doogee/f7/PlatformConfig.mk
+# Platform
+TARGET_BOARD_PLATFORM := mt6797
+TARGET_LDPRELOAD += libxlog.so
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_BOARD_SUFFIX := _64
+TARGET_BOARD_PLATFORM_GPU := mali-t880mp4
+TARGET_USES_64_BIT_BINDER := true
+
+# Architecture Extensions
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_VFP := true
+
+# Inherit from doogee f7 device
+PRODUCT_MAKEFILES := \
+    device/doogee/f7/device_f7.mk
 
 # Device vendor board
 -include vendor/doogee/f7/BoardConfigVendor.mk
